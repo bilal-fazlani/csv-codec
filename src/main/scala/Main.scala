10e328c305c3f4aa34e7ee1959b429c9e81c6bb3
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-06-19 23:50:26 +0530

    manual Person codecs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,6 +13,11 @@ given Decoder[Int] = new {
     str.toIntOption.toRight(s"$str is not a valid Int")
 }
 
+given Decoder[Boolean] = new {
+  def decode(str: String): Either[String, Boolean] =
+    str.toBooleanOption.toRight(s"$str is not a valid Boolean")
+}
+
 trait Encoder[T]:
   extension (value: T) def encode: String
 
@@ -24,9 +29,41 @@ given Encoder[Int] = new {
   extension (value: Int) def encode: String = value.toString
 }
 
+given Encoder[Boolean] = new {
+  extension (value: Boolean) def encode: String = value.toString
+}
+
+case class Person(name: String, age: Int, employee: Boolean)
+
+given Encoder[Person] = new {
+  extension (value: Person)
+    def encode: String =
+      List(
+        value.name.encode,
+        value.age.encode,
+        value.employee.encode
+      ).mkString(",")
+}
+
+given Decoder[Person] = new {
+  def decode(str: String): Either[String, Person] =
+    str.split(",").toList match {
+      case nameStr :: ageStr :: empStr :: Nil =>
+        for {
+          age <- Decoder[Int].decode(ageStr)
+          emp <- Decoder[Boolean].decode(empStr)
+        } yield (Person(nameStr, age, emp))
+      case _ => Left(s"[$str could not be parsed into Person]")
+    }
+}
+
 @main def hello: Unit =
   println(1232.encode)
   println("asds".encode)
   println(Decoder[Int].decode("123"))
   println(Decoder[String].decode("asd12323"))
   println(Decoder[Int].decode("asd12323"))
+
+  val person = Person("bilal", 20, true).encode
+  println(person.encode)
+  println(Decoder[Person].decode("bilal,20,true"))

4d18c2e6f113e65f89aff696f945ccf27aeb4353
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-06-22 01:27:02 +0530

    all tests passing. use shapeless encoder by default

--- src/main/scala/com/bilalfazlani/Encoder.scala
+++ src/main/scala/com/bilalfazlani/Encoder.scala
@@ -1,46 +1,44 @@
 package com.bilalfazlani
 
-import scala.deriving.*
-import scala.compiletime.{erasedValue, summonInline}
+import shapeless3.deriving.*
 
 trait Encoder[T]:
   extension (value: T) def encode: String
+  private[bilalfazlani] def fieldSize: Int = 1
+
 object Encoder {
   def apply[T: Encoder] = summon[Encoder[T]]
-  inline def summonAll[T <: Tuple]: List[Encoder[_]] =
-    inline erasedValue[T] match
-      case _: EmptyTuple => Nil
-      case dd: (t *: ts) => summonInline[Encoder[t]] :: summonAll[ts]
 
-  def sumEncoder[T](
-      s: Mirror.SumOf[T],
-      encoders: => List[Encoder[_]]
-  ): Encoder[T] =
-    new Encoder[T]:
-      extension (t: T)
-        def encode: String =
-          val index   = s.ordinal(t)
-          val encoder = encoders(index)
-          encoder.asInstanceOf[Encoder[Any]].encode(t)
+  given product[T](using
+      inst: K0.ProductInstances[Encoder, T],
+      labelling: Labelling[T]
+  ): Encoder[T] = new {
+    extension (value: T)
+      def encode: String = if (labelling.elemLabels.nonEmpty)
+        Range(0, labelling.elemLabels.length)
+          .map { i =>
+            inst.project(value)(i)(
+              [t] => (enc: Encoder[t], x: t) => enc.encode(x)
+            )
+          }
+          .mkString(",")
+      else labelling.label
 
-  def productEncoder[T](encoders: => List[Encoder[_]]): Encoder[T] = // (1)
-    new Encoder[T]: // (2)
-      extension (t: T)
-        def encode: String =
-          (t.asInstanceOf[Product]
-            .productIterator)
-            .zip(encoders.iterator)
-            .map { case (p, s) =>
-              s.asInstanceOf[Encoder[Any]]
-                .encode(p)
-            }
-            .mkString(",")
+    private[bilalfazlani] override def fieldSize: Int =
+      labelling.elemLabels.length
+  }
 
-  inline given derived[T](using m: Mirror.Of[T]): Encoder[T] =
-    lazy val encoders = summonAll[m.MirroredElemTypes]
-    inline m match
-      case s: Mirror.SumOf[T]     => sumEncoder(s, encoders)
-      case _: Mirror.ProductOf[T] => productEncoder(encoders)
+  given coproduct[T](using
+      inst: K0.CoproductInstances[Encoder, T],
+      labelling: Labelling[T]
+  ): Encoder[T] = new {
+    extension (value: T)
+      def encode: String =
+        inst.ordinal(value).asInstanceOf[Encoder[Any]].encode(value)
+  }
+
+  inline def derived[T](using gen: K0.Generic[T]): Encoder[T] =
+    gen.derive(product, coproduct)
 }
 
 given Encoder[String] = a => a
@@ -49,4 +47,45 @@ given Encoder[Int] = a => a.toString
 
 given Encoder[Boolean] = a => a.toString
 
-given [T: Encoder]: Encoder[Option[T]] = a => a.fold("")(_.encode)
+given [T: Encoder]: Encoder[Option[T]] = new {
+  private lazy val enc = Encoder[T]
+  extension (value: Option[T])
+    def encode: String =
+      value.fold(List.fill(enc.fieldSize)("").mkString(","))(_.encode)
+  private[bilalfazlani] override def fieldSize: Int = enc.fieldSize
+}
+
+//-------------- TRY OUT -----------------
+
+case class Age(years: Int, months: Int)
+case class Person(
+    name: String,
+    bla: Option[Option[Option[Int]]],
+    middleName: Option[String],
+    city: String,
+    age: Age,
+    parentAge: Option[Age],
+    employeeType: EmployeeType,
+    department: Option[Department]
+) derives Encoder
+
+enum EmployeeType derives Encoder:
+  case Permanent, Contractor
+
+enum Department derives Encoder:
+  case Marketing
+  case Sales
+  case Custom(departmentId: Int, departmentName: String)
+
+@main def hello12 =
+  val p = Person(
+    "bilal",
+    Some(Some(Some(3))),
+    None,
+    "mumbai",
+    Age(20, 12),
+    None,
+    EmployeeType.Contractor,
+    None
+  )
+  println(p.encode)

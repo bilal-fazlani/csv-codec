9787424521f5dddd3b5bef95556b6cbf3a696e0b
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-06-22 01:21:49 +0530

    rename project + add option decoder

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ val scala3Version = "3.0.0"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "metacsv",
+    name := "csv-scala",
     version := "0.1.0",
     scalaVersion := scala3Version,
     libraryDependencies ++= Seq(
--- src/main/scala/com/bilalfazlani/ShapelessEncoder.scala
+++ src/main/scala/com/bilalfazlani/ShapelessEncoder.scala
@@ -4,6 +4,7 @@ import shapeless3.deriving.*
 
 trait ShapelessEncoder[T]:
   extension (value: T) def encode: String
+  private[bilalfazlani] def fieldSize: Int = 1
 
 object ShapelessEncoder {
   def apply[T: ShapelessEncoder] = summon[ShapelessEncoder[T]]
@@ -11,22 +12,30 @@ object ShapelessEncoder {
   given product[T](using
       inst: K0.ProductInstances[ShapelessEncoder, T],
       labelling: Labelling[T]
-  ): ShapelessEncoder[T] = a =>
-    if (labelling.elemLabels.nonEmpty)
-      Range(0, labelling.elemLabels.length)
-        .map { i =>
-          inst.project(a)(i)(
-            [t] => (enc: ShapelessEncoder[t], x: t) => enc.encode(x)
-          )
-        }
-        .mkString(",")
-    else labelling.label
+  ): ShapelessEncoder[T] = new {
+    extension (value: T)
+      def encode: String = if (labelling.elemLabels.nonEmpty)
+        Range(0, labelling.elemLabels.length)
+          .map { i =>
+            inst.project(value)(i)(
+              [t] => (enc: ShapelessEncoder[t], x: t) => enc.encode(x)
+            )
+          }
+          .mkString(",")
+      else labelling.label
+
+    private[bilalfazlani] override def fieldSize: Int =
+      labelling.elemLabels.length
+  }
 
   given coproduct[T](using
       inst: K0.CoproductInstances[ShapelessEncoder, T],
       labelling: Labelling[T]
-  ): ShapelessEncoder[T] = a =>
-    inst.ordinal(a).asInstanceOf[ShapelessEncoder[Any]].encode(a)
+  ): ShapelessEncoder[T] = new {
+    extension (value: T)
+      def encode: String =
+        inst.ordinal(value).asInstanceOf[ShapelessEncoder[Any]].encode(value)
+  }
 
   inline def derived[T](using gen: K0.Generic[T]): ShapelessEncoder[T] =
     gen.derive(product, coproduct)
@@ -38,6 +47,11 @@ given ShapelessEncoder[Int] = a => a.toString
 
 given ShapelessEncoder[Boolean] = a => a.toString
 
-given [T: ShapelessEncoder]: ShapelessEncoder[Option[T]] = a =>
-  a.fold("")(_.encode)
+given [T: ShapelessEncoder]: ShapelessEncoder[Option[T]] = new {
+  private lazy val enc = ShapelessEncoder[T]
+  extension (value: Option[T])
+    def encode: String =
+      value.fold(List.fill(enc.fieldSize)("").mkString(","))(_.encode)
+  private[bilalfazlani] override def fieldSize: Int = enc.fieldSize
+}
 
